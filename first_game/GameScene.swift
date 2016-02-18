058c268a9bdb3340bb81ce346fa469b8b7b33d24
Saman Amarasinghe <saman@lcs.mit.edu>
2016-02-17 20:47:00 -0500

    now full screen.

--- first_game/GameScene.swift
+++ first_game/GameScene.swift
@@ -11,7 +11,8 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         //let box = SKSpriteNode(imageNamed: "Bag_Black_Box_Sprite")
-        let box = SKSpriteNode(imageNamed: "Spaceship")
+       // let box = SKSpriteNode(imageNamed: "Spaceship")
+        let box = SKSpriteNode(color: UIColor(red:0.0, green:0.0, blue:0.0, alpha:1), size:CGSize(width: CGRectGetWidth(self.frame), height:CGRectGetHeight(self.frame)))
         let location = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
         box.position = location
         
@@ -21,25 +22,7 @@ class GameScene: SKScene {
         self.addChild(box)
     }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
-    }
+
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
